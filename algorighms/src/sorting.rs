f597d27200888536e37564d4ce8ec54d7c7711ac
m1ss1onBest <m1poisthbexxxx@gmail.com>
2025-03-19 19:56:53 +0200

    insretion-sort

--- algorighms/src/sorting.rs
+++ algorighms/src/sorting.rs
@@ -7,3 +7,13 @@ pub fn bubble_sort<T: Ord>(slice: &mut[T]) {
         }
     }
 }
+
+pub fn insertion_sort<T: Ord>(slice: &mut [T]) {
+    for i in 1..slice.len() {
+        let mut j = i;
+        while j > 0 && slice[j - 1] > slice[j] {
+            slice.swap(j - 1, j);
+            j -= 1;
+        }
+    }
+}
