0c973c9960124c8fd5aea675185ef7469ea9c360
m1ss1onBest <m1poisthbexxxx@gmail.com>
2025-03-21 13:01:51 +0200

    todo...

--- algorighms/src/search.rs
+++ algorighms/src/search.rs
@@ -7,25 +7,39 @@ pub fn linear_search<'a, T: Ord>(slice: &'a [T], elem: &T) -> Option<&'a T> {
     None
 }
 
-pub fn binary_search<'a, T: Ord>(slice: &'a [T], elem: &T) -> Option<&'a T> {
-    if slice.is_empty() {
-        return None;
-    }
+// pub fn binary_search<'a, T: Ord>(slice: &'a [T], elem: &T) -> Option<&'a T> {
+//     if slice.is_empty() {
+//         return None;
+//     }
     
-    let mut left = 0;
-    let mut right = slice.len() - 1;
+//     let mut left = 0;
+//     let mut right = slice.len() - 1;
 
-    while left <= right {
-        let mid = left + (right - left) / 2;
-        let mid_val = &slice[mid];
+//     while left <= right {
+//         let mid = left + (right - left) / 2;
+//         let mid_val = &slice[mid];
 
-        if mid_val < elem {
-            left = mid + 1;
-        } else if mid_val > elem {
-            right = mid.saturating_sub(1);
-        } else {
-            return Some(mid_val);
-        }
-    }
-    None
-}
+//         if mid_val < elem {
+//             left = mid + 1;
+//         } else if mid_val > elem {
+//             right = mid.saturating_sub(1);
+//         } else {
+//             return Some(mid_val);
+//         }
+//     }
+//     None
+// }
+
+// pub fn interpolation_search<'a, T: Ord>(slice: &'a [T], to_find: &T) -> Option<&'a T> {
+//     let mut low = 0;
+//     let mut high = slice.len() - 1;
+
+//     while slice[low] < *to_find && slice[high] > *to_find {
+//         if slice[high] == slice[low] {
+//             break;
+//         }
+
+//         let mid = low + ((to_find - slice[low]) * (high - low));
+//     }
+//     None
+// }
